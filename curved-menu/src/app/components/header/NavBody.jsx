66f8b0024ba6df62b4446b97b8e30277ed29189f
MylesGit <37464485+Sayinq@users.noreply.github.com>
2024-01-03 19:07:09 -0500

    New menu icon, icon animations, etc

--- curved-menu/src/app/components/header/NavBody.jsx
+++ curved-menu/src/app/components/header/NavBody.jsx
@@ -2,44 +2,64 @@
 import styles from './style.module.css'
 import { useEffect, useState } from 'react';
 import Nav from './nav';
-import { AnimatePresence } from 'framer-motion';
+import Magnetic from '../magnetic';
+import { AnimatePresence, motion } from 'framer-motion';
+import { Spiral as Hamburger } from 'hamburger-react';
 import { usePathname } from 'next/navigation';
 
 export default function Home() {
-
   const [isActive, setIsActive] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
   const pathname = usePathname();
 
-  useEffect( () => {
-    if(isActive) setIsActive(false)
+  useEffect(() => {
+    if (isActive) setIsActive(false)
   }, [pathname])
+
+  const handleHover = () => {
+    setIsHovered(true);
+  };
+
+  const handleHoverEnd = () => {
+    setIsHovered(false);
+  };
 
   return (
     <>
-    <div className={styles.main}>
+      <div className={styles.main}>
 
-      <div className="fixed top-0 right-0 p-4 z-[2]">
-        <div onClick={() => {setIsActive(!isActive)}} className="flex items-center justify-center w-fit h-fit rounded-full cursor-pointer">
-          <div className={`${isActive ? styles.burgerActive : ""}`}>
-            <div>
-              <button class="relative group">
-                <div class="relative flex items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all bg-slate-700 duration-200 shadow-md">
-                  <div class="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 group-focus:-rotate-[45deg] origin-center">
-                    <div class="bg-white h-[2px] w-1/2 rounded transform transition-all duration-300 group-focus:-rotate-90 group-focus:h-[1px] origin-right delay-75 group-focus:-translate-y-[1px]"></div>
-                    <div class="bg-white h-[1px] rounded"></div>
-                    <div class="bg-white h-[2px] w-1/2 rounded self-end transform transition-all duration-300 group-focus:-rotate-90 group-focus:h-[1px] origin-left delay-75 group-focus:translate-y-[1px]"></div>
-                  </div>
-                </div>
-              </button>
+        <div className="fixed top-0 right-0 p-4 z-[2]">
+          <Magnetic>
+            <div
+              onClick={() => { setIsActive(!isActive) }}
+              className={`flex items-center justify-center w-[50px] h-[50px] rounded-full cursor-pointer pointer-events-auto hover:scale-[1.15] transition-all duration-75 ease-in-out ${
+                isHovered ? 'bg-[#148CEE]' : ''
+              }`}
+              onMouseEnter={handleHover}
+              onMouseLeave={handleHoverEnd}
+            >
+              <motion.div
+                className={styles.burgerActive}
+                initial={false}
+                animate={isActive ? 'open' : 'closed'}
+                exit="closed"
+              >
+                <Hamburger
+                  toggled={isActive}
+                  toggle={() => { setIsActive(!isActive) }}
+                  color={isActive ? '#ffffff' : (isHovered ? '#ffffff' : '#292929')}
+                />
+              </motion.div>
             </div>
-          </div>
+          </Magnetic>
         </div>
-      </div>
 
-    </div>
-    <AnimatePresence mode="wait">
-      {isActive && <Nav />}
-    </AnimatePresence>
+      </div>
+      <AnimatePresence mode="wait">
+        {isActive && <Nav />}
+      </AnimatePresence>
     </>
   )
 }
+
+
